8d6318a6dc34cb147e388cd9ad78dd3218135e24
Waleed Hosam <152779925+waleed-hosam@users.noreply.github.com>
2024-03-24 16:51:15 +0200

    Update system-info.py

--- system-info.py
+++ system-info.py
@@ -2,6 +2,10 @@ import psutil
 import cpuinfo
 import platform
 import wmi
+import sys
+import os
+import locale
+import ctypes
 
 win_num = platform.win32_ver()[0]
 win_edition = platform.win32_edition()
@@ -10,9 +14,12 @@ device_name = platform.node()
 ram = f'{psutil.virtual_memory().total / 1024 / 1024 / 1024:.2f}'
 pc = wmi.WMI()
 video = pc.Win32_VideoController()[0].name
+windll = ctypes.windll.kernel32
 
-print(f"Device name : {device_name.upper()}")
-print(f"Operating System : Windows {win_num} {win_edition[:3]} {win_or[:2]}-{win_or[2:]}")
+print(f"Computer : {device_name.upper()}")
+print(f"OS : Windows {win_num} {win_edition[:3]} {win_or[:2]}-{win_or[2:]}")
+print(f"Architecture : x{platform.architecture()[0][:2]}")
+print(f'System Language : {locale.windows_locale[windll.GetUserDefaultUILanguage()]}')
 print(f'Processor : {cpuinfo.get_cpu_info()["brand_raw"]}')
 print(f"Installed Memory (RAM) : {ram} GB")
 print(f"Video Card : {video}")
